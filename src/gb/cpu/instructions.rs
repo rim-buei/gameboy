66e8a08aff902487dfda210ddb7a4179e09acb00
Rim <rim.buei@gmail.com>
2018-10-24 23:02:31 +0900

    gb/cpu/instructions: initial import

---
+++ src/gb/cpu/instructions.rs
@@ -0,0 +1,11 @@
+use super::super::ram::Ram;
+
+use super::registers::Registers;
+
+pub fn exec(opcode: u8, reg: &mut Registers, ram: &mut Ram) -> (u8, u8) {
+    (0, 0)
+}
+
+pub fn exec_ex(opcode: u8, reg: &mut Registers, ram: &mut Ram) -> (u8, u8) {
+    (0, 0)
+}
