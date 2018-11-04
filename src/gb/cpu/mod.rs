ca9f72fce573a37e3909e2150ecbd670e53239e8
Rim <rim.buei@gmail.com>
2018-11-04 18:32:05 +0900

    gb/cpu: remove unnecessary methods

--- src/gb/cpu/mod.rs
+++ src/gb/cpu/mod.rs
@@ -12,7 +12,7 @@ use self::register::Register8;
 use self::register::Registers;
 
 pub fn step(reg: &mut Registers, ram: &mut Ram) -> u8 {
-    let addr = reg.get_PC();
+    let addr = reg.PC;
     let opcode = ram.read(addr);
 
     let (bytes, cycles) = if opcode != 0xCB {
@@ -20,29 +20,16 @@ pub fn step(reg: &mut Registers, ram: &mut Ram) -> u8 {
         exec(opcode, reg, ram)
     } else {
         // 2-byte instruction
-        let addr = reg.inc_PC().get_PC();
+        let addr = reg.PC.wrapping_add(1);
         let opcode = ram.read(addr);
 
         exec_ex(opcode, reg, ram)
     };
 
-    reg.add16(Register16::PC, bytes as u16);
+    reg.PC = reg.PC.wrapping_add(bytes as u16);
     cycles
 }
 
 pub fn reset(reg: &mut Registers) {
     *reg = Registers::new()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_reset() {
-        let mut reg = Registers::new();
-        reg.set8(Register8::A, 1);
-        reset(&mut reg);
-        assert_eq!(0, reg.get8(Register8::A));
-    }
 }
