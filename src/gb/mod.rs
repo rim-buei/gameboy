59b7381e7ae5e7f702026c2925c7e549034423c4
Rim <rim.buei@gmail.com>
2018-12-01 21:57:14 +0900

    gb: implement some basic functions for GameBoy

--- src/gb/mod.rs
+++ src/gb/mod.rs
@@ -8,8 +8,43 @@ mod cartridge;
 mod interrupt;
 mod ram;
 
+use self::cpu::Cpu;
+use self::mmu::Mmu;
+use self::ppu::Ppu;
+use self::screen::Screen;
+
 pub struct GameBoy {
-    cpu: cpu::Cpu,
-    ppu: ppu::Ppu,
-    mmu: mmu::Mmu,
+    cpu: Cpu,
+    ppu: Ppu,
+    mmu: Mmu,
+    screen: Screen,
+}
+
+impl GameBoy {
+    pub fn new() -> Self {
+        let mut mmu = Mmu::new();
+        mmu.load(0, rom());
+
+        GameBoy {
+            cpu: Cpu::new(),
+            ppu: Ppu::new(),
+            mmu: mmu,
+            screen: Screen::new(),
+        }
+    }
+
+    pub fn step(&mut self) {
+        loop {
+            let cycle = self.cpu.step(&mut self.mmu);
+            self.ppu.step(&mut self.mmu, cycle);
+            if self.ppu.is_screen_prepared() {
+                break;
+            }
+        }
+    }
+
+    pub fn screen(&mut self) -> Vec<u8> {
+        self.screen.refresh(&self.ppu.transfer_screen());
+        self.screen.dump()
+    }
 }
