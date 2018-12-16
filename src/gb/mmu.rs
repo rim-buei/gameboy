306e62d4462f516d3699f6b68bd9c358b455c081
Rim <rim.buei@gmail.com>
2018-12-16 16:11:08 +0900

    gb/mmu: rename Mmu::array to memory, remove Mmu::dump()

--- src/gb/mmu.rs
+++ src/gb/mmu.rs
@@ -3,14 +3,14 @@ use super::cartridge::Cartridge;
 use super::ram::Ram;
 
 pub struct Mmu {
-    array: Ram,
+    memory: Ram,
     cart: Cartridge,
 }
 
 impl Mmu {
     pub fn new() -> Self {
         Mmu {
-            array: Ram::new(vec![0x00; 1 << 16]),
+            memory: Ram::new(vec![0x00; 1 << 16]),
             cart: Cartridge::new(vec![0x00; 1 << 15]),
         }
     }
@@ -25,51 +25,47 @@ impl Mmu {
         self.cart = cart;
     }
 
-    pub fn dump(&self) -> Vec<u8> {
-        self.array.dump()
-    }
-
     pub fn simulate_bootloader(&mut self) {
-        self.array = Ram::new(vec![0x00; 1 << 16]);
-        self.array.write8(0xFF05, 0x00);
-        self.array.write8(0xFF06, 0x00);
-        self.array.write8(0xFF07, 0x00);
-        self.array.write8(0xFF10, 0x80);
-        self.array.write8(0xFF11, 0xBF);
-        self.array.write8(0xFF12, 0xF3);
-        self.array.write8(0xFF14, 0xBF);
-        self.array.write8(0xFF16, 0x3F);
-        self.array.write8(0xFF17, 0x00);
-        self.array.write8(0xFF19, 0xBF);
-        self.array.write8(0xFF1A, 0x7F);
-        self.array.write8(0xFF1B, 0xFF);
-        self.array.write8(0xFF1C, 0x9F);
-        self.array.write8(0xFF1E, 0xBF);
-        self.array.write8(0xFF20, 0xFF);
-        self.array.write8(0xFF21, 0x00);
-        self.array.write8(0xFF22, 0x00);
-        self.array.write8(0xFF23, 0xBF);
-        self.array.write8(0xFF24, 0x77);
-        self.array.write8(0xFF25, 0xF3);
-        self.array.write8(0xFF26, 0xF1);
-        self.array.write8(0xFF40, 0x91);
-        self.array.write8(0xFF42, 0x00);
-        self.array.write8(0xFF43, 0x00);
-        self.array.write8(0xFF45, 0x00);
-        self.array.write8(0xFF47, 0xFC);
-        self.array.write8(0xFF48, 0xFF);
-        self.array.write8(0xFF49, 0xFF);
-        self.array.write8(0xFF4A, 0x00);
-        self.array.write8(0xFF4B, 0x00);
-        self.array.write8(0xFFFF, 0x00);
+        self.memory = Ram::new(vec![0x00; 1 << 16]);
+        self.memory.write8(0xFF05, 0x00);
+        self.memory.write8(0xFF06, 0x00);
+        self.memory.write8(0xFF07, 0x00);
+        self.memory.write8(0xFF10, 0x80);
+        self.memory.write8(0xFF11, 0xBF);
+        self.memory.write8(0xFF12, 0xF3);
+        self.memory.write8(0xFF14, 0xBF);
+        self.memory.write8(0xFF16, 0x3F);
+        self.memory.write8(0xFF17, 0x00);
+        self.memory.write8(0xFF19, 0xBF);
+        self.memory.write8(0xFF1A, 0x7F);
+        self.memory.write8(0xFF1B, 0xFF);
+        self.memory.write8(0xFF1C, 0x9F);
+        self.memory.write8(0xFF1E, 0xBF);
+        self.memory.write8(0xFF20, 0xFF);
+        self.memory.write8(0xFF21, 0x00);
+        self.memory.write8(0xFF22, 0x00);
+        self.memory.write8(0xFF23, 0xBF);
+        self.memory.write8(0xFF24, 0x77);
+        self.memory.write8(0xFF25, 0xF3);
+        self.memory.write8(0xFF26, 0xF1);
+        self.memory.write8(0xFF40, 0x91);
+        self.memory.write8(0xFF42, 0x00);
+        self.memory.write8(0xFF43, 0x00);
+        self.memory.write8(0xFF45, 0x00);
+        self.memory.write8(0xFF47, 0xFC);
+        self.memory.write8(0xFF48, 0xFF);
+        self.memory.write8(0xFF49, 0xFF);
+        self.memory.write8(0xFF4A, 0x00);
+        self.memory.write8(0xFF4B, 0x00);
+        self.memory.write8(0xFFFF, 0x00);
 
-        self.array.write8(0xFF50, 0x01);
+        self.memory.write8(0xFF50, 0x01);
     }
 
     fn dma_transfer(&mut self, value: u8) {
         let start_addr = (value as u16) * 0x100;
         for i in 0..0xA0 {
-            self.array.write8(0xFE00 + i, self.array.read8(start_addr + i));
+            self.memory.write8(0xFE00 + i, self.memory.read8(start_addr + i));
         }
     }
 }
@@ -81,9 +77,9 @@ impl Bus for Mmu {
             0xA000...0xBFFF => self.cart.read(addr),
 
             // Mirror of 0xC000...0xDDFF (Typically not used)
-            0xE000...0xFDFF => self.array.read8(addr - 0x2000),
+            0xE000...0xFDFF => self.memory.read8(addr - 0x2000),
 
-            _ => self.array.read8(addr),
+            _ => self.memory.read8(addr),
         }
     }
 
@@ -97,11 +93,11 @@ impl Bus for Mmu {
             0xA000...0xBFFF => self.cart.write(addr, data),
 
             // Mirror of 0xC000...0xDDFF (Typically not used)
-            0xE000...0xFDFF => self.array.write8(addr - 0x2000, data),
+            0xE000...0xFDFF => self.memory.write8(addr - 0x2000, data),
 
             0xFF46 => self.dma_transfer(data),
 
-            _ => self.array.write8(addr, data),
+            _ => self.memory.write8(addr, data),
         };
     }
 
