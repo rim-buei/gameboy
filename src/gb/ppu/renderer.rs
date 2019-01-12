190bd24ebf0fba3ebec812eb15a112145aa874e8
Rim <rim.buei@gmail.com>
2019-01-12 20:46:55 +0900

    gb/ppu/renderer: implement sprite renderer

--- src/gb/ppu/renderer.rs
+++ src/gb/ppu/renderer.rs
@@ -25,13 +25,14 @@ impl<'a, B: Bus + 'a> Renderer<'a, B> {
         }
 
         if control.obj_enabled() {
-            self.render_obj_scanline(y);
+            self.render_sprites_scanline(y);
         }
     }
 
     fn render_background_scanline(&mut self, y: u8) {
         let tiles_loc = LCDControl::new(LCDC.read(self.bus)).bgwin_tile_loc();
         let map_loc = LCDControl::new(LCDC.read(self.bus)).bg_map_loc();
+        let palette = BGP.read(self.bus);
 
         let scroll_x = SCX.read(self.bus);
         let scroll_y = SCY.read(self.bus);
@@ -56,7 +57,7 @@ impl<'a, B: Bus + 'a> Renderer<'a, B> {
             let byte2 = self.bus.read8(tile_loc + 1);
 
             let color_bit = 7 - ((x_adjusted % 8) as u8);
-            let (r, g, b) = self.pick_rgb(color_bit, byte1, byte2);
+            let (r, g, b) = self.pick_rgb(palette, color_bit, byte1, byte2);
 
             self.frame_buffer.set_pixel(x, y, Pixel(r, g, b, 255));
         }
@@ -66,6 +67,7 @@ impl<'a, B: Bus + 'a> Renderer<'a, B> {
     fn render_window_scanline(&mut self, y: u8) {
         let tiles_loc = LCDControl::new(LCDC.read(self.bus)).bgwin_tile_loc();
         let map_loc = LCDControl::new(LCDC.read(self.bus)).win_map_loc();
+        let palette = BGP.read(self.bus);
 
         let window_x = WX.read(self.bus).wrapping_sub(7);
         let window_y = WY.read(self.bus);
@@ -93,22 +95,69 @@ impl<'a, B: Bus + 'a> Renderer<'a, B> {
             let byte2 = self.bus.read8(tile_loc + 1);
 
             let color_bit = 7 - ((x_adjusted % 8) as u8);
-            let (r, g, b) = self.pick_rgb(color_bit, byte1, byte2);
+            let (r, g, b) = self.pick_rgb(palette, color_bit, byte1, byte2);
 
             self.frame_buffer.set_pixel(x, y, Pixel(r, g, b, 255));
         }
     }
 
-    fn render_obj_scanline(&mut self, y: u8) {}
+    fn render_sprites_scanline(&mut self, y: u8) {
+        let y = y as i16;
+        let tiles_loc = 0x8000;
+        let sprite_height = LCDControl::new(LCDC.read(self.bus)).obj_height() as i16;
 
-    fn pick_rgb(&mut self, bit: u8, byte1: u8, byte2: u8) -> (u8, u8, u8) {
+        let palette0 = OBP0.read(self.bus);
+        let palette1 = OBP1.read(self.bus);
+
+        for sprite_n in 0..40 {
+            let offset: u16 = sprite_n * 4;
+
+            let sprite_x = (self.bus.read8(0xFE00 + offset + 1) as i16) - 8;
+            let sprite_y = (self.bus.read8(0xFE00 + offset) as i16) - 16;
+            if sprite_y > y || (sprite_y + sprite_height) <= y {
+                continue;
+            }
+
+            let attrs = self.bus.read8(0xFE00 + offset + 3);
+            let palette = if attrs & (1 << 4) != 0 { palette1 } else { palette0 };
+            let x_flip = attrs & (1 << 5) != 0;
+            let y_flip = attrs & (1 << 6) != 0;
+
+            let tile_y = if y_flip {
+                sprite_height - 1 - y - sprite_y
+            } else {
+                y - sprite_y
+            };
+
+            let tile_n = self.bus.read8(0xFE00 + offset + 2) as u16;
+            let tile_loc = tiles_loc + (tile_n * 16) + (tile_y * 2) as u16;
+
+            let byte1 = self.bus.read8(tile_loc);
+            let byte2 = self.bus.read8(tile_loc + 1);
+
+            for tile_x in 0..8 {
+                let x = sprite_x + tile_x;
+                if x < 0 || SCREEN_W as i16 <= x {
+                    continue;
+                }
+
+                let color_bit = if x_flip { tile_x } else { 7 - tile_x };
+                let (r, g, b) = self.pick_rgb(palette, color_bit as u8, byte1, byte2);
+                if r == PALETTE[0].0 {
+                    continue;
+                }
+
+                self.frame_buffer.set_pixel(x as u8, y as u8, Pixel(r, g, b, 255));
+            }
+        }
+    }
+
+    fn pick_rgb(&mut self, palette: u8, bit: u8, byte1: u8, byte2: u8) -> (u8, u8, u8) {
         let lo = (byte1 & (1 << bit) != 0) as u8;
         let hi = (byte2 & (1 << bit) != 0) as u8;
 
-        let color_num = (hi << 1) | lo;
-
-        let palette = BGP.read(self.bus);
-        let color = ((palette >> (color_num * 2)) & 0b11) as usize;
+        let color_n = (hi << 1) | lo;
+        let color = ((palette >> (color_n * 2)) & 0b11) as usize;
 
         PALETTE[color]
     }
