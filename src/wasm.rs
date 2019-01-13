7c75fd4961745d77853cc5d615bba67c68ce6d73
Rim <rim.buei@gmail.com>
2019-01-14 03:59:03 +0900

    wasm: add an event listener for keyboard inputs

--- src/wasm.rs
+++ src/wasm.rs
@@ -4,6 +4,7 @@ extern crate stdweb;
 mod gb;
 
 use self::gb::cartridge::Cartridge;
+use self::gb::joypad::Button;
 use self::gb::screen::{SCREEN_H, SCREEN_W};
 use self::gb::GameBoy;
 use std::cell::RefCell;
@@ -11,7 +12,7 @@ use std::rc::Rc;
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
 use stdweb::web;
-use stdweb::web::event::{ChangeEvent, ProgressLoadEvent};
+use stdweb::web::event::{ChangeEvent, KeyDownEvent, KeyUpEvent, ProgressLoadEvent};
 use stdweb::web::html_element::{CanvasElement, InputElement};
 use stdweb::web::{document, CanvasRenderingContext2d, FileList, FileReader, FileReaderResult};
 
@@ -21,41 +22,7 @@ macro_rules! enclose {
     }
 }
 
-fn main() {
-    stdweb::initialize();
-
-    let canvas: CanvasElement = document()
-        .query_selector("canvas")
-        .unwrap()
-        .unwrap()
-        .try_into()
-        .unwrap();
-    let ctx: CanvasRenderingContext2d = canvas.get_context().unwrap();
-
-    let gameboy = Rc::new(RefCell::new(GameBoy::new()));
-    add_load_rom_event_handler(gameboy.clone());
-    async_render_loop(ctx, gameboy.clone());
-
-    stdweb::event_loop();
-}
-
-fn async_render_loop(ctx: CanvasRenderingContext2d, gameboy: Rc<RefCell<GameBoy>>) {
-    web::window().request_animation_frame(move |_| {
-        let screen = gameboy.borrow_mut().step();
-
-        js! {
-            @{&ctx}.putImageData(new ImageData(
-                Uint8ClampedArray.from(@{screen}),
-                @{SCREEN_W},
-                @{SCREEN_H},
-            ), 0, 0);
-        }
-
-        async_render_loop(ctx, gameboy);
-    });
-}
-
-fn add_load_rom_event_handler(gameboy: Rc<RefCell<GameBoy>>) {
+fn handle_custom_rom(gameboy: Rc<RefCell<GameBoy>>) {
     let load_rom_button = web::document().get_element_by_id("load-rom").unwrap();
     load_rom_button.add_event_listener(move |event: ChangeEvent| {
         let input: InputElement = event.target().unwrap().try_into().unwrap();
@@ -81,4 +48,75 @@ fn add_load_rom_event_handler(gameboy: Rc<RefCell<GameBoy>>) {
 
         reader.read_as_array_buffer(&file).unwrap();
     });
+}
+
+fn handle_input(gameboy: Rc<RefCell<GameBoy>>) {
+    let handler = |key: &str| -> Option<Button> {
+        match key.to_lowercase().as_ref() {
+            "z" => Some(Button::A),
+            "x" => Some(Button::B),
+
+            "enter" => Some(Button::Start),
+            "shift" => Some(Button::Select),
+
+            "arrowup" => Some(Button::Up),
+            "arrowdown" => Some(Button::Down),
+            "arrowleft" => Some(Button::Left),
+            "arrowright" => Some(Button::Right),
+
+            _ => None,
+        }
+    };
+
+    web::window().add_event_listener(enclose!([gameboy] move |event: KeyDownEvent| {
+        match handler(&event.key()) {
+            Some(button) => {
+                gameboy.borrow_mut().press(button);
+            }
+            None => (),
+        }
+    }));
+    web::window().add_event_listener(enclose!([gameboy] move |event: KeyUpEvent| {
+        match handler(&event.key()) {
+            Some(button) => {
+                gameboy.borrow_mut().release(button);
+            }
+            None => (),
+        }
+    }));
+}
+
+fn async_render_loop(ctx: CanvasRenderingContext2d, gameboy: Rc<RefCell<GameBoy>>) {
+    web::window().request_animation_frame(move |_| {
+        let screen = gameboy.borrow_mut().step();
+
+        js! {
+            @{&ctx}.putImageData(new ImageData(
+                Uint8ClampedArray.from(@{screen}),
+                @{SCREEN_W},
+                @{SCREEN_H},
+            ), 0, 0);
+        }
+
+        async_render_loop(ctx, gameboy);
+    });
+}
+
+fn main() {
+    stdweb::initialize();
+
+    let gameboy = Rc::new(RefCell::new(GameBoy::new()));
+    handle_custom_rom(gameboy.clone());
+    handle_input(gameboy.clone());
+
+    let canvas: CanvasElement = document()
+        .query_selector("canvas")
+        .unwrap()
+        .unwrap()
+        .try_into()
+        .unwrap();
+    let ctx: CanvasRenderingContext2d = canvas.get_context().unwrap();
+    async_render_loop(ctx, gameboy.clone());
+
+    stdweb::event_loop();
 }
